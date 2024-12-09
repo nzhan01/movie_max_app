56b71a75518cf08efb454ca7304dac694f3e5fcc
Nicholas Zhang <nicholas@Nicholass-MacBook-Air-4.local>
2024-12-09 15:24:19 -0500

    added docstrings to watchlist modfel

--- meal_max/models/watchlist_model.py
+++ meal_max/models/watchlist_model.py
@@ -10,6 +10,7 @@ configure_logger(logger)
 
 
 class Watchlist(db.Model):
+    
     __tablename__ = 'watchlist'
 
     id = db.Column(db.Integer, primary_key=True)  # Added primary key for Watchlist
@@ -25,6 +26,23 @@ class Watchlist(db.Model):
 
     @staticmethod
     def add_to_watchlist(user_id, movie_id, title, overview=None, release_date=None, popularity=0.0):
+        """
+        Adds a movie to the user's watchlist.
+
+        Args:
+            user_id (int): ID of the user.
+            movie_id (int): ID of the movie (from TMDB).
+            title (str): Title of the movie.
+            overview (str, optional): Overview/summary of the movie. Defaults to None.
+            release_date (str, optional): Release date of the movie. Defaults to None.
+            popularity (float, optional): Popularity score of the movie. Defaults to 0.0.
+
+        Returns:
+            None
+
+        Raises:
+            IntegrityError: If the movie cannot be added to the watchlist due to a database error.
+        """
         entry = Watchlist(
             user_id=user_id,
             movie_id=movie_id,
@@ -38,6 +56,22 @@ class Watchlist(db.Model):
 
     @staticmethod
     def get_watchlist(username: str) -> list:
+        """
+        Retrieves the watchlist for a given user.
+
+        Args:
+            username (str): The username of the user.
+
+        Returns:
+            list: A list of dictionaries containing watchlist entries with keys:
+                  - "id": ID of the watchlist entry.
+                  - "movie_title": Title of the movie.
+                  - "added_on": Date and time the movie was added.
+                  - "watched": Boolean indicating if the movie has been watched.
+
+        Raises:
+            ValueError: If the user is not found in the database.
+        """
         user = Users.query.filter_by(username=username).first()
         if not user:
             logger.error("User '%s' not found.", username)
@@ -52,6 +86,19 @@ class Watchlist(db.Model):
 
     @staticmethod
     def remove_from_watchlist(username: str, movie_title: str) -> dict:
+        """
+        Removes a movie from the user's watchlist.
+
+        Args:
+            username (str): The username of the user.
+            movie_title (str): The title of the movie to be removed.
+
+        Returns:
+            dict: A message indicating the movie was removed, with the movie title.
+
+        Raises:
+            ValueError: If the user is not found or the movie is not in the user's watchlist.
+        """
         user = Users.query.filter_by(username=username).first()
         if not user:
             logger.error("User '%s' not found.", username)
